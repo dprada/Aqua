457b23bc5c9ec50cd669c729cabccf4b6fe1a9e9
Diego Prada <Diego.Prada@frias.uni-freiburg.de>
2013-03-05 17:52:24 +0100

    Initialization

--- io_formats/io_formats.py
+++
@@ -1,7 +0,0 @@
-from frame import *
-import coor_pdb as coor_pdb
-import coor_gro as coor_gro
-import coor_xtc as coor_xtc
-import coor_trr as coor_trr
-import coor_bin as coor_bin # Should be deprecated
-import coor_dcd as coor_dcd
