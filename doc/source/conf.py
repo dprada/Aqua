8e068a9bdf3b82bbabb2f587d2e7c1f12b85b96b
Diego Prada <Diego.Prada@frias.uni-freiburg.de>
2013-04-18 12:46:10 +0200

    Doc in process

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -52,8 +52,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Aqua'
-copyright = u'2012, Diego Prada-Gracia'
+project = u'AquaLab'
+copyright = u'2013, Diego Prada-Gracia'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -103,7 +103,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'misphinxdoc'
+html_theme = 'minature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,7 +122,7 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/logo_aqualab2.png'
+html_logo = '_static/logo_medium.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
